41c52883a39658a86dd7464c4f5f753a2d0ee6a4
371tti <minaiseiya@gmail.com>
2026-04-05 01:05:51 +0900

    fix: aws_lc_rs 依存を排除できなかったのでclang linkerを使用

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ async fn main() {
     dotenv::dotenv().ok();
     env_logger::try_init_from_env(env_logger::Env::default().default_filter_or("debug"))
         .unwrap_or(());
-    rustls::crypto::ring::default_provider()
+    rustls::crypto::aws_lc_rs::default_provider()
         .install_default()
         .expect("failed to install rustls crypto provider");
 
